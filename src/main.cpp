8cbf309b14537c8bc8fc85594de7b40e9dee7366
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2017-03-13 21:13:56 -0400

    commented the opengl calls

--- src/main.cpp
+++ src/main.cpp
@@ -115,18 +115,30 @@ int main(int argc, char** argv)
 	};
 
 	GLuint VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	// generate name for a new array
+	glGenVertexArrays(1, &VAO); // vertex array, store the references to the 
+	// generate a name for a new buffer
+	glGenBuffers(1, &VBO); // vertex buffer, list of numbers, the list of vertices
+	glGenBuffers(1, &EBO); // element buffer, the indices of the vertices that should be drawn together
+	// make VAO the active array, creating it if necessary
 	glBindVertexArray(VAO);
 
+	// make VBO the active buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// copies vertices from CPU to GPU as VBO, only once
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
+	// make EBO the active buffer
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// copies indices from CPU to GPU as EBO, only once
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
 
+	// how ans where to get the data
+	// 0 = attribute 0
+	// 3, GL_FLOAT = attribute has 3 components that are floats
+	// 3 * sizeof(GLfloat), (GLvoid*)0 = size of the component, starting at 0
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	// enables the attribute at 0, the same 0 that is the first in the above call to glVertexAttribPointer()
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -139,9 +151,11 @@ int main(int argc, char** argv)
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// draw here
+		// run the shader program
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
+
+		// draw the data
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
@@ -149,6 +163,7 @@ int main(int argc, char** argv)
 		glfwSwapBuffers(window);
 	}
 
+	// deletes the data from the array and buffers
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
